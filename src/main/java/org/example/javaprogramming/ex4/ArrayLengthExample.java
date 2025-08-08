3c98064e67bbaa09669502cb826c889e12b12249
김재원 <dumdum7@hs.ac.kr>
2025-08-08 17:04:12 +0900

    Feat: add ArrayLengthExample

---
+++ src/main/java/org/example/javaprogramming/ex4/ArrayLengthExample.java
@@ -0,0 +1,13 @@
+package org.example.javaprogramming.ex4;
+
+public class ArrayLengthExample {
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4, 5};
+
+        System.out.println(nums.length);
+
+    }
+
+}
