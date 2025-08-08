65b23ddfb3fbcc296b52b224291631227b105958
김재원 <dumdum7@hs.ac.kr>
2025-08-08 14:47:09 +0900

    add ex3

---
+++ src/main/java/org/example/javaprogramming/ex3/ImprovedForLoop.java
@@ -0,0 +1,15 @@
+package org.example.javaprogramming.ex3;
+
+public class ImprovedForLoop {
+
+    public static void main(String[] args) {
+
+        int[] scores = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int s : scores) {
+            System.out.println(s * 100);
+        }
+
+    }
+
+}
