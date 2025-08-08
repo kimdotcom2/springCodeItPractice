4fd9e4c8421ad6bf58b5bd8c51d7544f47739d21
김재원 <dumdum7@hs.ac.kr>
2025-08-08 16:42:03 +0900

    Fix: fix deep copy

--- src/main/java/org/example/javaprogramming/ex4/DeepCopyArray.java
+++ src/main/java/org/example/javaprogramming/ex4/DeepCopyArray.java
@@ -4,16 +4,22 @@ public class DeepCopyArray {
 
     public static void main(String[] args) {
 
-        int[] original = {1, 2, 3};
+        int[] original = {1, 2, 3, 4, 5};
         int[] copy = new int[original.length];
 
         for (int i = 0; i < original.length; i++) {
             copy[i] = original[i];
         }
 
-        copy[0] = 100;
+        for (int i = 0; i < copy.length; i++) {
+            copy[i] = original[i] + 100;
+        }
 
         System.out.println(original[0]);
+
+        for (int i = 0; i < copy.length; i++) {
+            System.out.println(copy[i]);
+        }
 
     }
 
