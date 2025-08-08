d6b8c8580e753bba45754d798f1053abd3d3f814
김재원 <dumdum7@hs.ac.kr>
2025-08-08 15:07:54 +0900

    add while

---
+++ src/main/java/org/example/javaprogramming/ex3/DoWhileExample.java
@@ -0,0 +1,16 @@
+package org.example.javaprogramming.ex3;
+
+public class DoWhileExample {
+
+    public static void main(String[] args) {
+
+        int i = 5;
+
+        do {
+            System.out.println("i = " + i);
+            i--;
+        } while (i > 0);
+
+    }
+
+}
---
+++ src/main/java/org/example/javaprogramming/ex3/WhileExample.java
@@ -0,0 +1,16 @@
+package org.example.javaprogramming.ex3;
+
+public class WhileExample {
+
+    public static void main(String[] args) {
+
+        int count = 5;
+
+        while (count > 0) {
+            System.out.println("count = " + count);
+            count--;
+        }
+
+    }
+
+}
