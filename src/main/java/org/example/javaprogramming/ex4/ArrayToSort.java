737ae701d259071cd78300d5f9dbfbf00483cb59
김재원 <dumdum7@hs.ac.kr>
2025-08-08 17:08:37 +0900

    Feat: add ArrayToSort

---
+++ src/main/java/org/example/javaprogramming/ex4/ArrayToSort.java
@@ -0,0 +1,16 @@
+package org.example.javaprogramming.ex4;
+
+import java.util.Arrays;
+
+public class ArrayToSort {
+
+    public static void main(String[] args) {
+
+        int[] data = {1, 2, 3, 4, 5};
+        Arrays.sort(data);
+
+        System.out.println(Arrays.toString(data));
+
+    }
+
+}
