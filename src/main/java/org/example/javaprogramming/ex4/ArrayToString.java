e1f04830e381f1619297ca12e1862456038b9b25
김재원 <dumdum7@hs.ac.kr>
2025-08-08 17:05:50 +0900

    Feat: add ArrayToString

---
+++ src/main/java/org/example/javaprogramming/ex4/ArrayToString.java
@@ -0,0 +1,15 @@
+package org.example.javaprogramming.ex4;
+
+import java.util.Arrays;
+
+public class ArrayToString {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+}
