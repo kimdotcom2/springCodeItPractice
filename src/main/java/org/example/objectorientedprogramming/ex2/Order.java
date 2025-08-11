2e4eb1d3671e42a5951cba39a44e13613f696825
김재원 <dumdum7@hs.ac.kr>
2025-08-11 14:40:24 +0900

    Feat: add Order, Product

---
+++ src/main/java/org/example/objectorientedprogramming/ex2/Order.java
@@ -0,0 +1,61 @@
+package org.example.objectorientedprogramming.ex2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+
+    private String orderId;
+    private List<Product> products;
+    private boolean isPaid;
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public boolean isPaid() {
+        return isPaid;
+    }
+
+    public void setPaid(boolean paid) {
+        isPaid = paid;
+    }
+
+    public Order() {
+        this.orderId = "NONE";
+        this.products = new ArrayList<>();
+        this.isPaid = false;
+    }
+
+    public Order(String orderId, List<Product> products) {
+        this.orderId = orderId;
+        this.products = products;
+        this.isPaid = false;
+    }
+
+    public Order(String orderId, Product product) {
+        this(orderId, new ArrayList<>());
+        this.products.add(product);
+    }
+
+    public Order(String orderId) {
+        this(orderId, new ArrayList<>());
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
